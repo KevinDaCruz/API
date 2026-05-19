9fd1e0be088d9bed1d3262107aaccaf6c64f62d7
KevinDaCruz <kevindc39@hotmail.com>
2026-05-19 10:28:18 +0200

    Activité 3

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,22 @@
 <?php
 
 use App\Http\Controllers\API\BookController;
+use App\Http\Controllers\API\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('books', BookController::class);
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'login']);
+
+Route::middleware('auth:sanctum')->post('/logout', [UserController::class, 'logout']);
+
+Route::get('/books', [BookController::class, 'index']);
+Route::get('/books/{book}', [BookController::class, 'show']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/books', [BookController::class, 'store']);
+    Route::put('/books/{book}', [BookController::class, 'update']);
+    Route::patch('/books/{book}', [BookController::class, 'update']);
+    Route::delete('/books/{book}', [BookController::class, 'destroy']);
+});
 
 Route::get('/ping', function () {
     return response()->json([
